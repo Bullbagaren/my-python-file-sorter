942cbb2a3aef1b6d41936dfee84fb4d90487bd45
Julius Ramshage <JuliusRamshage@lavabit.com>
2023-07-24 19:42:54 +0200

    it can now filter some folders too

--- filesorter.py
+++ filesorter.py
@@ -7,9 +7,12 @@ def main():
     folders_to_be_sorted = sys.argv
     del folders_to_be_sorted[0] 
     contents = os.listdir(folders_to_be_sorted[0])
+    for folder in folders_to_be_sorted:
 
-    relevant_files = [i for i in contents if i[0] != "."]
-    print(relevant_files)
+        not_hidden_files = [i for i in contents if i[0] != "."]
+        relevant_files = [i for i in not_hidden_files if os.path.isdir(folder +"/" + i) == False]
+        print(folder)
+        print(relevant_files)
 
 
 
