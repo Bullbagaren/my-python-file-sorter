e6eb9781782d206a8349573521b04da1004e0f92
Julius Ramshage <JuliusRamshage@lavabit.com>
2023-07-26 16:46:12 +0200

    now it should sort file to appropriate folders, haven't done a test run yet

--- filesorter.py
+++ filesorter.py
@@ -7,13 +7,30 @@ def main():
     folders_to_be_sorted = sys.argv
     del folders_to_be_sorted[0] 
     contents = os.listdir(folders_to_be_sorted[0])
-    for folder in folders_to_be_sorted:
+    not_hidden_files = [i for i in contents if i[0] != "." if os.path.isdir(folder + "/" + i) == False]
+    
+    for file in not_hidden_files: 
+        check_file_extention = file.splt(".")
+        match check_file_extention[1]:
+            case "jpg" or "png": 
+                os.system("mv " + folders_to_be_sorted + "/" + file + " " + "~/" + "Pictures")
 
-        not_hidden_files = [i for i in contents if i[0] != "."]
-        relevant_files = [i for i in not_hidden_files if os.path.isdir(folder +"/" + i) == False]
-        print(folder)
-        print(relevant_files)
+            case "txt" or "doc" or "docx" or "pdf":
 
+                os.system("mv " + folders_to_be_sorted + "/" + file + " " + "~/" + "Document")
+
+            case "mp3" or "flac" or "wav" or "wma":
+
+                os.system("mv " + folders_to_be_sorted + "/" + file + " " + "~/" + "Music")
+
+            case "mp4" or "webm" or "gif" or "mkv" or "avi":
+
+                os.system("mv " + folders_to_be_sorted + "/" + file + " " + "~/" + "Video")
+
+            
+
+
+    
 
 
 
